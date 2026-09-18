610a3001ccadf11f8a543b83e92d3f47aa94d77e
Quinn <quinn@erjinyi.com>
2026-09-18 19:56:58 +0800

    test(access): run publication ack contract in native suite

--- apps/browser/overlay/components/aegis_access/access_route_planner_native_test.cc
+++ apps/browser/overlay/components/aegis_access/access_route_planner_native_test.cc
@@ -9,6 +9,7 @@
 #include "components/aegis_access/access_route_planner_contract_test.h"
 #include "components/aegis_access/browser_request_metadata_seed_contract_test.h"
 #include "components/aegis_access/published_request_runtime_contract_test.h"
+#include "components/aegis_access/policy_publication_ack_tracker_contract_test.h"
 #include "components/aegis_access/request_dispatch_gate_contract_test.h"
 #include "components/aegis_access/request_generation_tuple_builder_contract_test.h"
 #include "components/aegis_access/request_ownership_registry_contract_test.h"
@@ -21,7 +22,8 @@ class NativeObserver : public aegis_access::test::ContractTestObserver,
                        public aegis_access::test::BaseProxyConfigGenerationStateTestObserver,
                        public aegis_access::test::IdentityGenerationStateTestObserver,
                        public aegis_access::test::ProxySelectionGenerationStateTestObserver,
-                       public aegis_access::test::RequestGenerationTupleBuilderTestObserver {
+                       public aegis_access::test::RequestGenerationTupleBuilderTestObserver,
+                       public aegis_access::test::PolicyPublicationAckTrackerTestObserver {
  public:
   void Expect(bool condition, const std::string& label) override {
     ++checks_;
@@ -65,6 +67,8 @@ int main() {
   aegis_access::test::RunBaseProxyConfigGenerationStateRegressionTests(observer);
   aegis_access::test::RunRequestGenerationTupleBuilderUnitTests(observer);
   aegis_access::test::RunRequestGenerationTupleBuilderRegressionTests(observer);
+  aegis_access::test::RunPolicyPublicationAckTrackerUnitTests(observer);
+  aegis_access::test::RunPolicyPublicationAckTrackerRegressionTests(observer);
   if (observer.failures() != 0) {
     std::cerr << "FAIL: aegis_access native unit (" << observer.failures()
               << " failures, " << observer.checks() << " checks)\n";
