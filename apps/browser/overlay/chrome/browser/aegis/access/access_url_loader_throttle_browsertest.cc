7a5b189e6ab2b3f709afa44dc87f348919e509af
Quinn <quinn@erjinyi.com>
2026-09-18 18:09:30 +0800

    build(access): include browser profile in acceptance test

--- apps/browser/overlay/chrome/browser/aegis/access/access_url_loader_throttle_browsertest.cc
+++ apps/browser/overlay/chrome/browser/aegis/access/access_url_loader_throttle_browsertest.cc
@@ -17,6 +17,7 @@
 #include "chrome/browser/aegis/access/access_published_request_runtime.h"
 #include "chrome/browser/net/profile_network_context_service.h"
 #include "chrome/browser/net/profile_network_context_service_factory.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
