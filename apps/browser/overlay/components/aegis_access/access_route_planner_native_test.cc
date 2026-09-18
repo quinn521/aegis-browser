9dcfb077e3aed966805f8688389f4e9a6df02de9
Quinn <quinn@erjinyi.com>
2026-09-18 17:06:07 +0800

    test(access): run selection generation in native suite

--- apps/browser/overlay/components/aegis_access/access_route_planner_native_test.cc
+++ apps/browser/overlay/components/aegis_access/access_route_planner_native_test.cc
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "components/aegis_access/access_identity_generation_state_contract_test.h"
+#include "components/aegis_access/access_proxy_selection_generation_state_contract_test.h"
 #include "components/aegis_access/access_route_planner_contract_test.h"
 #include "components/aegis_access/browser_request_metadata_seed_contract_test.h"
 #include "components/aegis_access/published_request_runtime_contract_test.h"
@@ -15,7 +16,8 @@ namespace {
 class NativeObserver : public aegis_access::test::ContractTestObserver,
                        public aegis_access::test::RequestOwnershipRegistryTestObserver,
                        public aegis_access::test::BrowserRequestMetadataSeedTestObserver,
-                       public aegis_access::test::IdentityGenerationStateTestObserver {
+                       public aegis_access::test::IdentityGenerationStateTestObserver,
+                       public aegis_access::test::ProxySelectionGenerationStateTestObserver {
  public:
   void Expect(bool condition, const std::string& label) override {
     ++checks_;
@@ -53,6 +55,8 @@ int main() {
   aegis_access::test::RunPublishedRequestRuntimeRegressionTests(observer);
   aegis_access::test::RunIdentityGenerationStateUnitTests(observer);
   aegis_access::test::RunIdentityGenerationStateRegressionTests(observer);
+  aegis_access::test::RunProxySelectionGenerationStateUnitTests(observer);
+  aegis_access::test::RunProxySelectionGenerationStateRegressionTests(observer);
   if (observer.failures() != 0) {
     std::cerr << "FAIL: aegis_access native unit (" << observer.failures()
               << " failures, " << observer.checks() << " checks)\n";
